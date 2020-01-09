0513b88e8a221fbc0794cfa55ad9cbf5d70b05f7
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-01-09 22:37:08 +0700

    Update test2.go

--- test2.go
+++ test2.go
@@ -7,11 +7,13 @@ type student struct {
 	age   int
 	email string
 }
+
 func main() {
 	var std [10]student
 	std[0] = student{"kao", 18, "kao_game@hotmail.co.th"}
 
 	fmt.Println(std[0])
 	fmt.Println(std[0].name)
+	fmt.Println(std[0].age)
+	fmt.Println(std[0].email)
 }
-
